254dee8a253361d375f4f4713c6c9c0c15186c17
Myung <god.godzilla.af@gmail.com>
2019-12-11 17:07:58 -0500

    index.html added -barebone .js only -index.js added -countdown problem, error, [output is null]

---
+++ index.js
@@ -0,0 +1,32 @@
+//MDN Looping code
+/* 	Loop from 10 down to 0
+	if 10 print"Countdown 10"
+	if 0 print"Blast Off!"
+	if other, print just number
+*/
+
+
+// let i = 10;
+
+// const para = document.createElement('p');  
+// para.textContent = ;
+// output.appendChild(para);
+
+output = document.querySelector('.output');
+output.innerHTML = "";
+
+function countDown(){
+	i = 10
+	while (i >= 0){
+		para = document.createElement('p');
+		if (i === 10){
+			para.textContent = ("Countdown " + i)
+		} else if (i === 0) {
+			para.textContent = ("Blast Off!")
+		} else {
+			para.textContent = i
+		}
+		output.appendChild(para);
+		i--;
+	}
+}
